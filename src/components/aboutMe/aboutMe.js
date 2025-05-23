ef57775adc7a4efc8946efe23b43ebc83022f7b6
Marneus981 <md.sora@hotmail.com>
2025-05-23 10:43:35 -0600

    Added end product files, started about me section

---
+++ src/components/aboutMe/aboutMe.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './aboutMe.css'
+import about_img from '../../assets/placeholder/about.png'
+import play_icon from '../../assets/placeholder/play_icon.png'
+
+const AboutMe = () => {
+  return (
+    <div className='about'>
+        <div className='about_left'>
+            <img src={about_img} alt="" className='about_img'/>             
+        </div>
+        <div className='about_right'>
+            <h3>About Me</h3>
+            <h2>Random Bloke Trynna Complete His Nostos</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Tenetur incidunt eius corrupti facilis, necessitatibus officia rerum assumenda qui,
+              quidem ut eveniet laudantium vel et iste temporibus dignissimos quam magnam iusto?</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default AboutMe
