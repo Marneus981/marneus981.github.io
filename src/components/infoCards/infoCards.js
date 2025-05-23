7cd2d5bf605293dd875ef40c3ec4d2e98cd44f1e
Marneus981 <md.sora@hotmail.com>
2025-05-23 01:23:51 -0600

    Implemented Infocards correctly

--- src/components/infoCards/infoCards.js
+++ src/components/infoCards/infoCards.js
@@ -4,7 +4,15 @@ import '../../config';
 
 const InfoCards = () => {
     return (
-    <section>
+    <section 
+        style={{
+            width: '100%',
+
+            paddingRight: '20%',
+            paddingLeft: '25%',
+            paddingTop: '5%',
+        }}
+    >
         <input className="sr-only" id="card-1" type="radio" name="panel" defaultChecked />
         <input className="sr-only" id="card-2" type="radio" name="panel" />
         <input className="sr-only" id="card-3" type="radio" name="panel" />
@@ -17,16 +25,14 @@ const InfoCards = () => {
             <div>
                 <img src="https://picsum.photos/id/641/200/200" alt="watch tower"/>
                 <div class="poem">
-                    <p>
-                        A lone watchtower stands by the moaning shore,<br />
-                        Its shadow long, though eyes watch no more.<br />
-                        Salt-kissed winds whisper secrets of the sea,<br />
+                    <p >
+                        A lone watchtower stands by the moaning shore,
+                        Its shadow long, though eyes watch no more.
+                        Salt-kissed winds whisper secrets of the sea
                         But no soul stirs in its silent decree.
                     </p>
-
                 </div>
             </div>
-
         </article>
 
         <article id="article-2">
@@ -38,9 +44,9 @@ const InfoCards = () => {
             <div>
                 <img src="https://picsum.photos/id/634/200/200" alt="forest mist"/>
                 <div class="poem">
-                    <p> The forest sleeps beneath a shroud of grey,<br />
-                        Where dawn forgets to find its way.<br />
-                        Each branch is cloaked in silver breath,<br />
+                    <p> The forest sleeps beneath a shroud of grey,
+                        Where dawn forgets to find its way.
+                        Each branch is cloaked in silver breath,
                         Suspended still, in quiet death.
                     </p>
                 </div>
@@ -58,9 +64,9 @@ const InfoCards = () => {
 
                 <div class="poem">
                     <p>
-                        There once was a pineapple, proud and round,<br />
-                        Who somehow sprouted in farming ground.<br />
-                        Among the wheat and rows of corn,<br />
+                        There once was a pineapple, proud and round,
+                        Who somehow sprouted in farming ground.
+                        Among the wheat and rows of corn,
                         He stood confused, alone, forlorn.
                     </p>
                 </div>
